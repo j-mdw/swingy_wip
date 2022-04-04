e4f9a63861f894804c89807773f1d87f0d67e28b
j-mdw <jmaydew@student.42.fr>
2022-04-04 12:55:51 -0700

    ✨ initial project setup

---
+++ src/main/java/com/github/j_mdw/app/App.java
@@ -0,0 +1,13 @@
+package com.github.j_mdw.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
