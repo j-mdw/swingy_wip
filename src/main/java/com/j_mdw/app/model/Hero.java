aeee548c00a82d09f82cedaf1f9bf874baf5d86e
j-mdw <jmaydew@student.42.fr>
2022-04-06 12:07:04 -0700

    ✨ +hero attributes

---
+++ src/main/java/com/j_mdw/app/model/Hero.java
@@ -0,0 +1,11 @@
+package com.j_mdw.app.model;
+
+public class Hero {
+  private String name;
+  private String type;
+  private int level;
+  private int xp;
+  private int attack;
+  private int defense;
+  private int hitPoints;
+}
